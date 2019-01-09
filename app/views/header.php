20f6595ad3c8f9ded04c22ea0f92f73da7fe43f4
Nguyen Huu Nghia <you@example.com>
2019-01-09 07:47:33 +0700

    Responsive and update Display!

--- app/views/header.php
+++ app/views/header.php
@@ -5,9 +5,12 @@
 	<title>LeoSi Note</title>
 	<!-- Bootstrap CSS -->
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+	<!-- Header CSS -->
 	<link rel="stylesheet" href="public/css/styleHeader.css">
 	<!-- Main CSS -->
 	<link rel="stylesheet" type="text/css" href="public/css/main.css">
+	<!-- Reponsive -->
+	<link rel="stylesheet" type="text/css" href="public/css/responsive.css">
 	<!-- Navagation bar  -->
 	<?php require('navbar.php'); ?>
 <body>
